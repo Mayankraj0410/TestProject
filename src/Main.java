991f6e1ec85842220c64d89c50003f00821eea9f
Mayank Raj <rajmayank0410@gmail.com>
2026-03-15 14:20:02 +0530

    trying commit from ide

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 
         Main m = new Main();
-        m.setName("Mayank");
+        m.setName("fish");
         System.out.println(m.getName());
     }
 }
